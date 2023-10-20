3ade92a84114fdba478d0140b6145862d5621b0a
Patryk Kubala <patrykkubala1999@gmail.com>
2023-10-20 21:37:32 +0200

    init

---
+++ src/main/java/com/motivey/MotiveyApplication.java
@@ -0,0 +1,13 @@
+package com.motivey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotiveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotiveyApplication.class, args);
+	}
+
+}
